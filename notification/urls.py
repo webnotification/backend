bcc46ddb79fa1512f527d60f05d06676f6a60e7a
Ravi Kumar Nimmi <rvkmr102@gmail.com>
2015-12-22 19:53:46 +0530

    schema changed and delete group included

--- notification/urls.py
+++ notification/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^generate_user_id$', views.generate_user_id, name='generate_user_id'),
     url(r'^generate_group$', views.generate_group, name='generate_group'),
+    url(r'^delete_group$', views.delete_group, name='delete_group'),
     url(r'^save_push_key$', views.save_push_key, name='save_push_key'),
     url(r'^send_notification$', views.send_notification, name='send_notification'),
     url(r'^send_permission_response$', views.send_permission_response, name='send_permission_response'),
